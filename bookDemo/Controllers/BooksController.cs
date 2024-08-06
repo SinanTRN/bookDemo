74f132d1e3e177ca467e82d2816730bb4cb532ad
Sinan Turan <turansinan2004@gmail.com>
2024-08-06 10:02:58 +0300

    GET request defined

--- bookDemo/Controllers/BooksController.cs
+++ bookDemo/Controllers/BooksController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using bookDemo.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bookDemo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/books")]
     [ApiController]
     public class BooksController : ControllerBase
     {
-
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = AplicationContext.Books;
+            return Ok(books);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name ="id")]int id)
+        {
+            var book = AplicationContext.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+            if(book is null) return NotFound();//404
+            return Ok(book);
+        }
     }
 }
