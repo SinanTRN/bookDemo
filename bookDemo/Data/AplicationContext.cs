0649418976a726ba7bebefe70cc2e637b452d8ec
Sinan Turan <turansinan2004@gmail.com>
2024-08-06 09:42:56 +0300

    Proje dosyası ekle.

---
+++ bookDemo/Data/AplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class AplicationContext
+    {
+        public static List<Book>Books { get; set; }
+        static AplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book(){Id=1,Title="Şeker Portakalı",Price=100},
+                new Book(){Id=2,Title="Güneşi Uyandıralım",Price=75},
+                new Book(){Id=3,Title="Delifişek",Price=55}
+            };
+        }
+    }
+}
